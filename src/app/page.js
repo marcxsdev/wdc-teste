ccdbee8fed45d2787fe0e6380a36c23e44eaee6f
marcxsdev <marcosalexandre1322@gmail.com>
2025-09-09 13:46:16 -0300

    feat(ui): adicionando PpkaCard

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import CarouselSection from "@/components/sections/CarouselSection";
 import CTASection from "@/components/sections/CTASection";
 import HeroSection from "@/components/sections/HeroSection";
+import PpkaCard from "@/components/ui/PpkaCard";
 import Image from "next/image";
 
 export default function Home() {
---
+++ src/components/ui/PpkaCard.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+const PpkaCard = ({ text, bgColor }) => {
+  return (
+    <div
+      className={`${bgColor} flex flex-row items-center gap-5.5 w-81 h-15 lg:w-88 lg:h-16 rounded-lg`}
+    >
+      <div className="relative h-full w-11 lg:w-12 overflow-hidden rounded-lg ">
+        <Image
+          src="/images/ppka/Rectangle1599.png"
+          alt="Banner do card"
+          fill
+          className="object-cover"
+          quality={100}
+        />
+      </div>
+
+      <h4 className="text-deep-blue font-main text-lg lg:text-xl font-extrabold leading-4">
+        {text}
+      </h4>
+    </div>
+  );
+};
+
+export default PpkaCard;
